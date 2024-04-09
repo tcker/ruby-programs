765a41f68e21b6e34d87f0283f1d2e6362cc6d31
Ram <140935795+tcker@users.noreply.github.com>
2024-04-09 17:18:15 +0800

    Add files via upload

---
+++ Ruby Program/tempCodeRunnerFile.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'erb'
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+get '/' do
+  # Assume @name is set to "John"
+  @title = "RUBY INTEGRATION HTML"
+  @name2 = "Johny loyd 2"
+  @paragraph = "This is a paragraph"
+
+
+
+  #IMPORANT DO NOT REMOVE
+  # Read the ERB template from file
+  template = File.read('index.html.erb')
+
+  # Create an ERB instance
+  erb = ERB.new(template)
+
+  # Render the template with the current context
+  erb.result(binding)
+end
+  #IMPORANT DO NOT REMOVE
---
+++ Ruby Program/test.rb
@@ -0,0 +1,20 @@
+class Nigger
+    def name
+        "Test"
+    end
+end
+
+nigga_instance = Nigger.new
+puts nigga_instance.name    
+
+puts "Hello World"; puts "I love drinking coffee\n\n"
+
+print "Hello World"; print "I love drinking coffee\n\n"
+
+# has endline
+puts "Hello,"
+puts " World!"
+
+# has no endline
+print "Hello,"
+print " World!"
